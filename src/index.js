9d8fc8abc6ee5a7830bf9a06ba6e46283cdd4657
Habiba Mohamed <107395049+Habiba0079@users.noreply.github.com>
2024-09-16 23:10:23 +0300

    add props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,31 +49,74 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Footer/>
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
 function Header() {
-  return <h1>Mama Jons Pizza</h1>;
+  const headerStyle = {
+    // color: "red",
+    // fontSize: "48px",
+    // textTransform: "uppercase",
+  };
+  return (
+    <header className="header">
+      <h1 style={headerStyle}>Mama Jons Pizza</h1>;
+    </header>
+  );
 }
-function Menu() {}
-function Footer() {
-  return<footer> {new Date().toLocaleTimeString()}, We're currently open</footer>;
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        price={18}
+        photoName="pizzas/prosciutto.jpg"
+      />
+      <Pizza
+        name="Pizza Salamino"
+        ingredients="Tomato, mozarella, and pepperoni"
+        price={15}
+        photoName="pizzas/salamino.jpg"
+      />
+    </main>
+  );
 }
 
-function Pizza() {
+function Pizza(props) {
+  //console.log(props);
   return (
-    <div>
-      <img src="pizzas/prosciutto.jpg" alt="Pizza Prosciutto" />
-      <h2>Pizza Prosciutto</h2>
-      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
+  );
+}
+
+function Footer() {
+  const operHour = 10;
+  const closeHour = 22;
+
+  const hour = new Date().getHours();
+  const currentState =
+    hour >= operHour && hour <= closeHour
+      ? "We're currently open :)"
+      : "Sorry we are closed :'(";
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}, {currentState}
+    </footer>
   );
 }
 
